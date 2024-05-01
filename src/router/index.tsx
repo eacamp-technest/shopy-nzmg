830cc91c2d12c72e0f520f8ae8cd2632ac0fb489
eyvaz-gulnaz <gul.bax@mail.ru>
2024-05-01 18:02:31 +0400

    bbb

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -4,7 +4,7 @@ import { MainRouter } from './Main.Router'
 import { AuthRouter } from './Auth.Router'
 import { NavigationContainer } from '@react-navigation/native'
 
-const isAuth = false //change it later
+const isAuth = false //change it
 
 const Router = () => {
     return (<NavigationContainer>
