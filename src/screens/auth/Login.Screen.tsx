374d6523843ea3122a5f6d65d5e16b869db83164
NikoAlyv <enihad801@gmail.com>
2024-05-13 01:13:41 +0400

    fix:SplashScreen logo

--- src/screens/auth/Login.Screen.tsx
+++ src/screens/auth/Login.Screen.tsx
@@ -1,10 +1,12 @@
 import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
+import {SvgImage} from 'components/SvgImages';
 
 export const LoginScreen = () => {
   return (
     <View>
       <Text>Login.Screen</Text>
+      <SvgImage source={require('../../assets/vectors/logo.svg')} />
     </View>
   );
 };
