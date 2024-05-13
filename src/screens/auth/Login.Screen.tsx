52b0e4f6e80dc5357feeb083d074e1e311c8ea2c
NikoAlyv <enihad801@gmail.com>
2024-05-13 11:06:06 +0400

    fix:SplashScreen bacground

--- src/screens/auth/Login.Screen.tsx
+++ src/screens/auth/Login.Screen.tsx
@@ -1,14 +1,10 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {View, Text} from 'react-native';
 import React from 'react';
-import {SvgImage} from 'components/SvgImages';
 
 export const LoginScreen = () => {
   return (
     <View>
       <Text>Login.Screen</Text>
-      <SvgImage source={require('../../assets/vectors/logo.svg')} />
     </View>
   );
 };
-
-const styles = StyleSheet.create({});
