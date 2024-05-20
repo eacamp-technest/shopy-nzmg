2fa73ea5538b0afeb935e347bdd15eb22a86ded7
NikoAlyv <enihad801@gmail.com>
2024-05-20 08:42:33 +0400

    fix:type's error

--- src/Test.Screen.tsx
+++ src/Test.Screen.tsx
@@ -1,21 +1,18 @@
-import { CardPay } from 'components/CardPay';
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import { Navbar } from 'components/Navbar'
-import { FormRules } from 'constants/formRules';
-import { NativeStackScreenProps } from '@react-navigation/native-stack';
-import { NavigationParamList } from 'types/navigation.types';
-import { InputControlled } from 'components/InputControlled'
-import { Routes } from 'router/routes';
-import { colors } from 'theme/colors';
-import { useForm } from 'react-hook-form';
-
-
+import {CardPay} from 'components/CardPay';
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import {Navbar} from 'components/Navbar';
+import {FormRules} from 'constants/formRules';
+import {NativeStackScreenProps} from '@react-navigation/native-stack';
+import {NavigationParamList} from 'types/navigation.types';
+import {InputControlled} from 'components/InputControlled';
+import {Routes} from 'router/routes';
+import {colors} from 'theme/colors';
+import {useForm} from 'react-hook-form';
 
 export const TestScreen: React.FC<
-  NativeStackScreenProps<NavigationParamList, Routes.addnewcard>
-> = ({ navigation }) => {
-
+  NativeStackScreenProps<NavigationParamList, Routes.test>
+> = ({navigation}) => {
   return (
     <View style={styles.root}>
       <Text>Salam Aleikum</Text>
@@ -23,10 +20,9 @@ export const TestScreen: React.FC<
   );
 };
 
-
 const styles = StyleSheet.create({
   root: {
     flex: 1,
-    backgroundColor: "green"
+    backgroundColor: 'green',
   },
-})
+});
--- src/screens/auth/YourCard.Screen.tsx
+++ src/screens/auth/YourCard.Screen.tsx
@@ -3,13 +3,19 @@ import React from 'react';
 import {CardPay} from 'components/CardPay';
 import {Navbar} from 'components/Navbar';
 import {Buttons} from 'components/Buttons';
+import {NativeStackScreenProps} from '@react-navigation/native-stack';
+import {NavigationParamList} from 'types/navigation.types';
+import {Routes} from 'router/routes';
 
-export const YourCardScreen = () => {
+export const YourCardScreen: React.FC<
+  NativeStackScreenProps<NavigationParamList, Routes.yourCard>
+> = ({navigation}) => {
   return (
     <View style={styles.root}>
       <Navbar
         rootStyle={styles.navbar}
         leftActionType="icon"
+        onLeftPress={navigation.goBack}
         left={require('../../assets/vectors/chevron-left.svg')}
         title="YOUR CARD"
       />
