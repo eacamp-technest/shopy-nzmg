17a019ba7205cdd27c805878be31a3fd7dbd2839
Gulnaz Bakhshiyeva <135440551+eyvaz-gulnaz@users.noreply.github.com>
2024-05-20 12:22:53 +0400

    Update CreateYourCard.Screen.tsx

--- src/screens/auth/CreateYourCard.Screen.tsx
+++ src/screens/auth/CreateYourCard.Screen.tsx
@@ -1,35 +1,41 @@
-import {StyleSheet, View, Image} from 'react-native';
+import { StyleSheet, View, Image } from 'react-native';
 import React from 'react';
-import {NativeStackScreenProps} from '@react-navigation/native-stack';
-import {NavigationParamList} from 'types/navigation.types';
-import {Routes} from 'router/routes';
-import {Navbar} from 'components/Navbar';
-import {useForm} from 'react-hook-form';
-import {InputControlled} from 'components/InputControlled';
-import {FormRules} from 'constants/formRules';
-import {Buttons} from 'components/Buttons';
-import {IAddNewCard} from './AddNewCard.Screen';
-import {CardPay} from 'components/CardPay';
+import { NativeStackScreenProps } from '@react-navigation/native-stack';
+import { NavigationParamList } from 'types/navigation.types';
+import { Routes } from 'router/routes';
+import { Navbar } from 'components/Navbar';
+import { useForm } from 'react-hook-form';
+import { InputControlled } from 'components/InputControlled';
+import { FormRules } from 'constants/formRules';
+import { Buttons } from 'components/Buttons';
+import Card from '../../assets/images/PaymentCard.png'
+import { IAddNewCard } from './AddNewCard.Screen';
 
 // const Card = require('../../assets/images/PaymentCard.png'),
 
 export const CreateYourCardScreen: React.FC<
   NativeStackScreenProps<NavigationParamList, Routes.createyourcard>
-> = ({navigation}) => {
+> = ({ route, navigation }) => {
+  const params = route.params as IAddNewCard | undefined
+  const { cardNumber = "", cardHolder = "", date = "" } = params ?? {}
   const {
     control,
     handleSubmit,
-    formState: {errors, isSubmitting},
+    formState: { errors, isSubmitting },
   } = useForm<IAddNewCard>({
     defaultValues: {
-      cardNumber: __DEV__ ? '4532 1245 8765 2156' : '',
-      cardHolder: __DEV__ ? 'Brooklyn Simmons' : '',
-      date: __DEV__ ? '12 / 24  /  088' : '',
+      cardHolder,
+      cardNumber,
+      date
+      // cardNumber: __DEV__ ? '4532 1245 8765 2156' : '',
+      // cardHolder: __DEV__ ? 'Brooklyn Simmons' : '',
+      // date: __DEV__ ? '12 / 24  /  088' : '',
     },
   });
   const onSubmit = (data: IAddNewCard) => {
-    console.log('Card info added');
-  };
+    console.log("Card info added");
+
+  }
   return (
     <View style={styles.root}>
       <Navbar
@@ -38,29 +44,26 @@ export const CreateYourCardScreen: React.FC<
         title="YOUR CARD"
         onLeftPress={navigation.goBack}
       />
-      <CardPay
-        text="Universal Card"
-        userName="Brooklyn Simmons"
-        cardSave="12/24"
-        cardNumber={'4532 1245 8765 2156'}
-      />
+      <View>
+        <Image source={Card} style={styles.CardImage} />
+      </View>
       <View style={styles.InputView}>
         <InputControlled
           control={control}
-          name="card number"
+          name="cardNumber"
           label="Card Number"
           errorMessage={errors.cardNumber?.message}
           rules={FormRules.cardNumber}
           type="text"
-          placeholder="card number"
+          placeholder="card Number"
         />
         <InputControlled
           control={control}
-          name="card name"
+          name="cardHolder"
           label="Cardholder Name"
           errorMessage={errors.cardHolder?.message}
           rules={FormRules.cardHolder}
-          placeholder="Cardholder Name"
+          placeholder="card Holder"
         />
         <InputControlled
           control={control}
@@ -71,22 +74,26 @@ export const CreateYourCardScreen: React.FC<
           placeholder="date"
         />
       </View>
-      <Buttons style={styles.button} text="Save" />
+      <View style={styles.buttonView}>
+        <Buttons text='Save' />
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   root: {
-    gap: 24,
-    flex: 1,
+    gap: 24
   },
-
+  CardImage: {
+    width: '100%',
+    height: 200,
+    borderRadius: 16,
+  },
   InputView: {
-    paddingTop: 8,
-    gap: 24,
+    gap: 24
   },
-  button: {
-    marginTop: 94,
-  },
+  buttonView: {
+    marginTop: 120
+  }
 });
