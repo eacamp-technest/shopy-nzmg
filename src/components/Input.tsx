8bdaba01bf7ae6e82026527db16c40190867ee87
NikoAlyv <enihad801@gmail.com>
2024-06-24 18:16:22 +0400

    feat:filtered method

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,183 +1,198 @@
-import { StyleSheet, Text, View, TextInput, KeyboardTypeOptions, StyleProp, ViewStyle, Pressable } from 'react-native'
-import React, { useMemo, useState } from 'react'
-import { SvgImage } from './SvgImages'
-import { TypographyStyles } from 'theme/typography'
-import { colors } from 'theme/colors'
-import { standardHitSlopSize } from 'theme/consts.styles'
-import { CommonStyles } from 'theme/common.styles'
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  KeyboardTypeOptions,
+  StyleProp,
+  ViewStyle,
+  Pressable,
+} from 'react-native';
+import React, {useMemo, useState} from 'react';
+import {SvgImage} from './SvgImages';
+import {TypographyStyles} from 'theme/typography';
+import {colors} from 'theme/colors';
+import {standardHitSlopSize} from 'theme/consts.styles';
+import {CommonStyles} from 'theme/common.styles';
 
 export type TIcon = {
-    source: NodeRequire;
-    color?: string;
-    width?: number;
-    height?: number;
-    position?: 'left' | 'right';
-
-}
+  source: NodeRequire;
+  color?: string;
+  width?: number;
+  height?: number;
+  position?: 'left' | 'right';
+};
 export interface IInput {
-    type?: 'text' | 'phone' | 'password' | 'select';
-    label?: string;
-    caption?: string;
-    value?: string;
-    placeholder?: string;
-    disabled?: boolean;
-    keyboardType?: KeyboardTypeOptions;
-    icon?: TIcon | NodeRequire;
-    errorMessage?: string;
-    style?: StyleProp<ViewStyle>;
-    maxLength?: number;
-    setValue?: (value: string) => void;
-    onFocus?: () => void;
-    onBlur?: () => void;
-    onInputPress?: () => void;
-    noBorder?: boolean
+  type?: 'text' | 'phone' | 'password' | 'select';
+  label?: string;
+  caption?: string;
+  value?: string;
+  placeholder?: string;
+  disabled?: boolean;
+  keyboardType?: KeyboardTypeOptions;
+  icon?: TIcon | NodeRequire;
+  errorMessage?: string;
+  style?: StyleProp<ViewStyle>;
+  maxLength?: number;
+  setValue?: (value: string) => void;
+  onFocus?: () => void;
+  onBlur?: () => void;
+  onInputPress?: () => void;
+  noBorder?: boolean;
 }
 export const Input: React.FC<IInput> = ({
-    value,
-    type = "text",
-    setValue,
-    icon,
-    noBorder,
-    ...props
+  value,
+  type = 'text',
+  setValue,
+  icon,
+  noBorder,
+  ...props
 }) => {
-    const [focused, setFocused] = useState<boolean>(false)
-    const [secureTextEntry, setSecureTextEntry] = useState<boolean>(type === 'password')
+  const [focused, setFocused] = useState<boolean>(false);
+  const [secureTextEntry, setSecureTextEntry] = useState<boolean>(
+    type === 'password',
+  );
 
-    const isMoreIcon = useMemo(() => ('position' in (icon ?? {}) && (icon as TIcon)?.position === 'right') || type === 'password', [icon, type])
+  const isMoreIcon = useMemo(
+    () =>
+      ('position' in (icon ?? {}) && (icon as TIcon)?.position === 'right') ||
+      type === 'password',
+    [icon, type],
+  );
 
-    const isPressable = props.onInputPress instanceof Function
+  const isPressable = props.onInputPress instanceof Function;
 
-    const renderIcon = useMemo(() => {
-        if (type === 'password') {
-            return (
-                <Pressable hitSlop={standardHitSlopSize}>
-                    <SvgImage
-                        source={
-                            secureTextEntry
-                                ? require('../assets/vectors/eye-off.svg')
-                                : require('../assets/vectors/eye .svg')
-                        }
-                        color={colors.ink.base}
-                        width={24}
-                        height={24}
-                        onPress={() => setSecureTextEntry(state => !state)}
-                    />
-                </Pressable>
-            )
-        }
-        if (!icon) {
-            return null
-        }
-        if ('source' in icon) {
-            return (
-                <SvgImage
-                    source={icon.source}
-                    width={icon.width}
-                    color={icon.color}
-                    height={icon.height}
-                />
-            )
-        }
-        return (
-            <SvgImage
-                source={icon}
-                color={props.disabled ? colors.sky.base : colors.ink.base}
-            />
-        )
-    }, [icon, props.disabled, secureTextEntry, type])
-
-    const handleOnFocused = () => {
-        setFocused(true)
-        props?.onFocus?.()
-    }
-    const handleOnBlur = () => {
-        setFocused(false)
-        props?.onBlur?.()
-    }
-
-    return (
-        <View style={[styles.root, props?.style]}>
-            {props.label ? (
-                <Text style={TypographyStyles.RegularNoneSemiBold}>
-                    {props.label}
-                </Text>
-            ) : null
+  const renderIcon = useMemo(() => {
+    if (type === 'password') {
+      return (
+        <Pressable hitSlop={standardHitSlopSize}>
+          <SvgImage
+            source={
+              secureTextEntry
+                ? require('../assets/vectors/eye-off.svg')
+                : require('../assets/vectors/eye .svg')
             }
-            <View
-                style={[
-                    styles.wrapper,
-                    focused && styles.focused,
-                    props.disabled && styles.wrapperDisabled,
-                    isMoreIcon && CommonStyles.rowReverse,
-                    noBorder && styles.noBorder
-                ]}>
-                {renderIcon}
-                <TextInput
-                    placeholder={props.placeholder}
-                    keyboardType={props.keyboardType}
-                    value={value}
-                    onFocus={handleOnFocused}
-                    onBlur={handleOnBlur}
-                    onPressIn={props.onInputPress}
-                    autoCapitalize='none'
-                    maxLength={props.maxLength}
-                    // editable={!isPressable ?? !props.disabled}
-                    secureTextEntry={secureTextEntry}
-                    onChangeText={setValue}
-                    placeholderTextColor={props.disabled ? colors.sky.base : colors.ink.lighter}
-                    style={styles.input}
-                />
-            </View>
-            {props.caption || props.errorMessage ? (
-                <Text style={[
-                    styles.caption,
-                    props?.errorMessage ? styles.error : undefined
-                ]}>
-                    {props.errorMessage ?? props.caption}
-                </Text>
-            ) : null}
-        </View>
-    )
-}
+            color={colors.ink.base}
+            width={24}
+            height={24}
+            onPress={() => setSecureTextEntry(state => !state)}
+          />
+        </Pressable>
+      );
+    }
+    if (!icon) {
+      return null;
+    }
+    if ('source' in icon) {
+      return (
+        <SvgImage
+          source={icon.source}
+          width={icon.width}
+          color={icon.color}
+          height={icon.height}
+        />
+      );
+    }
+    return (
+      <SvgImage
+        source={icon}
+        color={props.disabled ? colors.sky.base : colors.ink.base}
+      />
+    );
+  }, [icon, props.disabled, secureTextEntry, type]);
+
+  const handleOnFocused = () => {
+    setFocused(true);
+    props?.onFocus?.();
+  };
+  const handleOnBlur = () => {
+    setFocused(false);
+    props?.onBlur?.();
+  };
+
+  return (
+    <View style={[styles.root, props?.style]}>
+      {props.label ? (
+        <Text style={TypographyStyles.RegularNoneSemiBold}>{props.label}</Text>
+      ) : null}
+      <View
+        style={[
+          styles.wrapper,
+          focused && styles.focused,
+          props.disabled && styles.wrapperDisabled,
+          isMoreIcon && CommonStyles.rowReverse,
+          noBorder && styles.noBorder,
+        ]}>
+        {renderIcon}
+        <TextInput
+          placeholder={props.placeholder}
+          keyboardType={props.keyboardType}
+          value={value}
+          onFocus={handleOnFocused}
+          onBlur={handleOnBlur}
+          onPressIn={props.onInputPress}
+          autoCapitalize="none"
+          maxLength={props.maxLength}
+          // editable={!isPressable ?? !props.disabled}
+          secureTextEntry={secureTextEntry}
+          onChangeText={setValue}
+          placeholderTextColor={
+            props.disabled ? colors.sky.base : colors.ink.lighter
+          }
+          style={styles.input}
+        />
+      </View>
+      {props.caption || props.errorMessage ? (
+        <Text
+          style={[
+            styles.caption,
+            props?.errorMessage ? styles.error : undefined,
+          ]}>
+          {props.errorMessage ?? props.caption}
+        </Text>
+      ) : null}
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    root: {
-        gap: 12
-    },
-    focused: {
-        borderWidth: 2,
-        borderColor: colors.primary.base
-    },
-    wrapperDisabled: {
-        backgroundColor: colors.sky.lighter,
-        borderColor: colors.sky.lighter,
-        color: colors.sky.base
-    },
-    error: {
-        color: colors.primary.base
-    },
-    caption: {
-        ...TypographyStyles.SmallNormalRegular,
-        color: colors.ink.lighter
-    },
-    wrapper: {
-        flexDirection: 'row',
-        alignItems: 'center',
-        borderWidth: 1,
-        borderColor: colors.sky.light,
-        borderRadius: 8,
-        paddingHorizontal: 16,
-        gap: 12,
-        height: 48
-    },
-    input: {
-        height: '100%',
-        flex: 1,
-        flexGrow: 1,
-        borderColor: 'red',
-        ...TypographyStyles.RegularNoneRegular
-    },
-    noBorder: {
-        borderWidth: 0
-    }
-})
+  root: {
+    gap: 12,
+  },
+  focused: {
+    borderWidth: 2,
+    borderColor: colors.primary.base,
+  },
+  wrapperDisabled: {
+    backgroundColor: colors.sky.lighter,
+    borderColor: colors.sky.lighter,
+    color: colors.sky.base,
+  },
+  error: {
+    color: colors.primary.base,
+  },
+  caption: {
+    ...TypographyStyles.SmallNormalRegular,
+    color: colors.ink.lighter,
+  },
+  wrapper: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    borderWidth: 1,
+    borderColor: colors.sky.light,
+    borderRadius: 8,
+    paddingHorizontal: 16,
+    gap: 12,
+    height: 48,
+  },
+  input: {
+    height: '100%',
+    flex: 1,
+    flexGrow: 1,
+    borderColor: 'red',
+    ...TypographyStyles.RegularNoneRegular,
+  },
+  noBorder: {
+    borderWidth: 0,
+  },
+});
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,23 +1,27 @@
-import React, { useCallback, useEffect, useState } from 'react';
-import { MainRouter } from './Main.Router';
-import { AuthRouter } from './Auth.Router';
-import { NavigationContainer } from '@react-navigation/native';
-import { useUserStore } from 'store/user/user.store';
-import BootSplash from 'react-native-bootsplash'
+import React, {useCallback, useEffect, useState} from 'react';
+import {MainRouter} from './Main.Router';
+import {AuthRouter} from './Auth.Router';
+import {NavigationContainer} from '@react-navigation/native';
+import {useUserStore} from 'store/user/user.store';
+import BootSplash from 'react-native-bootsplash';
 
 const delay = (ms: number, cb?: any) =>
   new Promise(resolve => setTimeout(resolve, ms, cb));
 
 const Router = () => {
-  const [ready, setReady] = useState<boolean>(false)
-  const { user, actions: { initialize } } = useUserStore(state => state)
+  const [ready, setReady] = useState<boolean>(false);
+  const {
+    user,
+    actions: {initialize},
+  } = useUserStore(state => state);
   const init = useCallback(async () => {
     await delay(1500, initialize());
     setReady(true);
-    await BootSplash.hide({ fade: true });
+    await BootSplash.hide({fade: true});
   }, [initialize]);
 
   useEffect(() => {
+    console.log(user);
     init();
   }, [init]);
 
