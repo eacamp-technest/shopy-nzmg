8745d9a958fd1cfb0ae6a0a25845ebf29a30db27
NikoAlyv <enihad801@gmail.com>
2024-06-26 11:06:05 +0400

    feat:DiscoverScreen

---
+++ src/constants/discover.ts
@@ -0,0 +1,28 @@
+import {colors} from 'theme/colors';
+
+export const discover = [
+  {
+    id: 0,
+    title: 'WOMEN',
+    image: require('../assets/images/Image.png'),
+    background: colors.primary.base,
+  },
+  {
+    id: 1,
+    title: 'MEN',
+    image: require('../assets/images/Image1.png'),
+    background: colors.blue.base,
+  },
+  {
+    id: 2,
+    title: 'KIDS',
+    image: require('../assets/images/Image2.png'),
+    background: colors.skyBlue.base,
+  },
+  {
+    id: 3,
+    title: 'KIDS',
+    image: require('../assets/images/Image3.png'),
+    background: colors.lavender.base,
+  },
+];
--- src/screens/main/Discover.Screen.tsx
+++ src/screens/main/Discover.Screen.tsx
@@ -1,10 +1,57 @@
-import {View, Text} from 'react-native';
+import {View, FlatList, Image, StyleSheet, Text, Pressable} from 'react-native';
 import React from 'react';
+import {discover} from 'constants/discover';
+import {Item} from 'react-native-paper/lib/typescript/components/Drawer/Drawer';
+import {CommonStyles} from 'theme/common.styles';
+import {TypographyStyles} from 'theme/typography';
+import {colors} from 'theme/colors';
+import {normalize} from 'theme/metrics';
 
 export const DiscoverScreen = () => {
   return (
-    <View>
-      <Text>DiscoverScreen</Text>
+    <View style={styles.root}>
+      <FlatList
+        data={discover}
+        showsVerticalScrollIndicator={false}
+        keyExtractor={item => item.id.toString()}
+        renderItem={({item}) => (
+          <Pressable
+            style={[
+              {
+                backgroundColor: `${item.background}`,
+              },
+              styles.contain,
+              CommonStyles.flexRow,
+            ]}>
+            <Text style={[TypographyStyles.title3, styles.title]}>
+              {item.title}
+            </Text>
+            <Image style={styles.image} source={item.image} />
+          </Pressable>
+        )}
+      />
     </View>
   );
 };
+const styles = StyleSheet.create({
+  root: {
+    flex: 1,
+    padding: 12,
+  },
+  contain: {
+    margin: 12,
+    borderRadius: 16,
+    overflow: 'hidden',
+  },
+  title: {
+    color: colors.white,
+    alignSelf: 'flex-end',
+    paddingHorizontal: normalize('horizontal', 24),
+    paddingBottom: 16,
+    position: 'absolute',
+  },
+  image: {
+    flexShrink: 1,
+    height: 184,
+  },
+});
